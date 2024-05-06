5d5f6f62571c91162bfc0fff6501978b7af17b9f
Kislev03 <kislev.espana@jala.university>
2024-05-06 17:44:26 -0600

    Fix conflicts

--- src/main/java/org/jala/university/presentation/LoansView.java
+++ src/main/java/org/jala/university/presentation/LoansView.java
@@ -6,14 +6,11 @@ import org.jala.university.commons.presentation.View;
 @Getter
 public enum LoansView {
     MAIN("main-view.fxml"),
-<<<<<<< src/main/java/org/jala/university/presentation/LoansView.java
-    LOAN_REQUEST_VIEW_PAGE1("loan-request-form-view.fxml"),
+
+    LOAN_REQUEST_VIEW("loan-request-form-view.fxml"),
     LOAN_REQUEST_VIEW_PAGE2("loan-request-form-page2-view.fxml"),
-    LOAN_REQUEST_VIEW_PAGE3("loan-request-form-page3-view.fxml");
+    LOAN_REQUEST_VIEW_PAGE3("loan-request-form-page3-view.fxml"),
     LOAN_LIST("loan-list-view.fxml");
-=======
-    LOAN_LIST("loan-list-view.fxml");
->>>>>>> src/main/java/org/jala/university/presentation/LoansView.java
 
     private final View view;
 
