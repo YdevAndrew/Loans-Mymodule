3369f25c8ce3744c0f44065fc9d1e8573bfd6f8d
André Hugo Santos Coelho <andre.santos@jala.university>
2024-11-09 23:00:38 +0000

    Revert "Merge branch 'Test-Controller-Main' into 'dev'"
    This reverts merge request !10

--- src/main/java/org/jala/university/MainApp.java
+++ src/main/java/org/jala/university/MainApp.java
@@ -1,11 +1,7 @@
 package org.jala.university;
 
 import org.jala.university.presentation.MainView;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EnableJpaRepositories(basePackages = "org.jala.university.domain.repository")
 public class MainApp {
     public static void main(String[] args) {
         MainView.launch(args);
--- src/main/java/org/jala/university/application/service/InstallmentEntityService.java
+++
@@ -1,9 +0,0 @@
-package org.jala.university.application.service;
-
-import java.util.List;
-
-import org.jala.university.application.dto.InstallmentEntityDto;
-
-public interface InstallmentEntityService {
-    List<InstallmentEntityDto> findByLoanId(Integer id);
-}
