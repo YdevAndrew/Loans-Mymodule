fa90191aec76adde5f1fe7ac69cd88186acc9eec
Fabiola Alves <neves.fabiolaf@gmail.com>
2024-11-10 12:53:06 -0300

    processo TDD: 2. implementação da funcionalidade

--- src/main/java/org/jala/university/utils/CalculationUtil.java
+++ src/main/java/org/jala/university/utils/CalculationUtil.java
@@ -1,4 +1,26 @@
 package org.jala.university.utils;
 
 public class CalculationUtil {
+
+    public static Double getTotalPayable(Double amountBorrowed, Double numberOfInstallments) {
+        Double totalPayable;
+        double monthlyInterestRate = 0.02;
+        // (1 + 0.02)^n (cálculo de juros compostos)
+        double factor = Math.pow(1 + monthlyInterestRate, numberOfInstallments);
+        // Valor total a pagar com juros compostos
+        totalPayable = amountBorrowed * factor;
+        return totalPayable;
+    }
+
+    public static Double getValueOfInstallments(Double amountBorrowed, Double numberOfInstallments) {
+        Double valueOfInstallments;
+        valueOfInstallments = getTotalPayable(amountBorrowed, numberOfInstallments) / numberOfInstallments;
+        return valueOfInstallments;
+    }
+
+    public static Double getTotalInterest(Double amountBorrowed, Double numberOfInstallments) {
+        Double totalInterest;
+        totalInterest = getTotalPayable(amountBorrowed, numberOfInstallments) - amountBorrowed;
+        return totalInterest;
+    }
 }
