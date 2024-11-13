aa32bf7a50660d6044384ab8183e5ec2fee31efa
André Hugo Santos Coelho <andre.santos@jala.university>
2024-11-13 17:31:41 +0000

    Update FormControllerTest.java

--- src/test/java/org/jala/university/controller/FormController/FormControllerTest.java
+++ src/test/java/org/jala/university/controller/FormController/FormControllerTest.java
@@ -16,6 +16,7 @@ import org.jala.university.presentation.controller.FormController;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
