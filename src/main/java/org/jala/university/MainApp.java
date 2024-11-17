46d21018ae207030f46d2123688f1fe00f21173e
Cleber Vinicius <cleber.carvalho@jala.university>
2024-11-17 20:32:47 -0300

    Implementation JavaDoc

--- src/main/java/org/jala/university/MainApp.java
+++ src/main/java/org/jala/university/MainApp.java
@@ -4,9 +4,20 @@ import org.jala.university.presentation.MainView;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * Main entry point for the University Loans Module Application.
+ * Combines Spring Boot and JavaFX to create a responsive and dynamic application.
+ */
 @SpringBootApplication
 @EnableScheduling
 public class MainApp {
+
+    /**
+     * The main method to launch the application.
+     * Delegates to the JavaFX {@link MainView} class for launching the UI.
+     *
+     * @param args the command-line arguments
+     */
     public static void main(String[] args) {
         MainView.launch(args);
     }
--- src/main/java/org/jala/university/utils/CalculationUtil.java
+++ src/main/java/org/jala/university/utils/CalculationUtil.java
@@ -1,7 +1,19 @@
 package org.jala.university.utils;
 
+/**
+ * Utility class for performing loan-related financial calculations.
+ * Includes methods for calculating total payable amount, installment value, and total interest.
+ */
 public class CalculationUtil {
 
+    /**
+     * Calculates the total amount payable based on the borrowed amount and the number of installments.
+     * Applies a compound interest rate of 2% per month.
+     *
+     * @param amountBorrowed      the principal amount borrowed
+     * @param numberOfInstallments the number of monthly installments
+     * @return the total amount payable with compound interest
+     */
     public static Double getTotalPayable(Double amountBorrowed, Double numberOfInstallments) {
         Double totalPayable;
         double monthlyInterestRate = 0.02;
@@ -12,12 +24,26 @@ public class CalculationUtil {
         return totalPayable;
     }
 
+    /**
+     * Calculates the value of each installment based on the borrowed amount and the number of installments.
+     *
+     * @param amountBorrowed      the principal amount borrowed
+     * @param numberOfInstallments the number of monthly installments
+     * @return the value of each installment
+     */
     public static Double getValueOfInstallments(Double amountBorrowed, Double numberOfInstallments) {
         Double valueOfInstallments;
         valueOfInstallments = getTotalPayable(amountBorrowed, numberOfInstallments) / numberOfInstallments;
         return valueOfInstallments;
     }
 
+    /**
+     * Calculates the total interest to be paid based on the borrowed amount and the number of installments.
+     *
+     * @param amountBorrowed      the principal amount borrowed
+     * @param numberOfInstallments the number of monthly installments
+     * @return the total interest to be paid
+     */
     public static Double getTotalInterest(Double amountBorrowed, Double numberOfInstallments) {
         Double totalInterest;
         totalInterest = getTotalPayable(amountBorrowed, numberOfInstallments) - amountBorrowed;
