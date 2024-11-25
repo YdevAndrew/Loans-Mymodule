db01b2a8f0e6b0a0be1b9c18870262efe68720b1
Allisia França <allisia.franca@jala.university>
2024-11-25 01:09:16 +0000

    Revert "Merge branch 'dev' into 'main'"
    This reverts merge request !37

--- src/main/java/org/jala/university/presentation/LoansView.java
+++ src/main/java/org/jala/university/presentation/LoansView.java
@@ -3,26 +3,14 @@ package org.jala.university.presentation;
 import lombok.Getter;
 import org.jala.university.commons.presentation.View;
 
-/**
- * Enum representing the views related to loans in the application.
- * Each enum constant corresponds to a specific FXML file.
- */
 @Getter
 public enum LoansView {
-
-    /**
-     * Represents the main view of the loans feature.
-     */
-    MAIN("Main-viewLoan/main-viewLoan.fxml");
+    MAIN("main-view.fxml");
 
     private final View view;
 
-    /**
-     * Constructor to associate a specific FXML file with the view.
-     *
-     * @param fileName the name of the FXML file associated with the view
-     */
     LoansView(String fileName) {
         this.view = new View(fileName);
     }
+
 }
